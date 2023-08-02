8cad3cfd0521c2f680757bd2ad858feefdb3b704
Adebukol <adebukolaomowole@gmail.com>
2023-08-03 00:45:50 +0100

    new update on js file

--- blogr.js
+++ blogr.js
@@ -1,1 +1,27 @@
-const dropDown= document.querySelector('.')
+const dropDown= document.querySelectorAll('.dropdown');
+
+console.log(viewLinks);
+const navLinks = document.getElementById('navlinks');
+console.log(navLinks);
+
+
+
+function mytion() {
+  if(document.getElementById('menuBar').style.display="block"){
+    navLinks.style.display="block";
+  }
+//   const viewLinks= document.getElementById('menuBar');
+// console.log(viewLinks);
+// const navLinks = document.getElementById('navlinks');
+// console.log(navLinks);
+
+  // console.log("clicked");
+  // 
+}
+mytion()
+function myFunction() {
+  let controller = "Hello, world!";
+  console.log(controller);
+}
+
+myFunction();
